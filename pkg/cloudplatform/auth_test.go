30d12c656f9539cbadedcaa08bb2bce04af05a9b
Luke Vear <vearluke@googlemail.com>
2017-08-31 18:59:18 +0100

    auth tests

--- pkg/cloudplatform/auth_test.go
+++ pkg/cloudplatform/auth_test.go
@@ -2,19 +2,20 @@ package cloudplatform
 
 import (
 	"testing"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
 	testPassword123 = "$2a$10$YZtNOffXRIekdOzILPokJuaX1Yn5qIi2bEY1kbPWAcTvdHl77dqca"
 )
 
-func init() {
-	//password := "patsPassword123"
-	//authUser.(User).
-}
-
 func TestGenPwHash(t *testing.T) {
-
+	password := "世界世界世界世界"
+	pwHash, _ := genPwHash(password)
+	if err := bcrypt.CompareHashAndPassword([]byte(pwHash), []byte(password)); err != nil {
+		t.Errorf("invalid password hash")
+	}
 }
 
 func TestLogin(t *testing.T) {
@@ -33,17 +34,34 @@ func TestLogin(t *testing.T) {
 	}
 }
 
-func TestGenerateTokenString(t *testing.T) {}
+func TestGenerateTokenStringParseJWT(t *testing.T) {
+	name := "thisWillBeTheSub"
+	user := NewUser(name)
 
-func TestParseJWT(t *testing.T) {
+	validToken, _ := generateTokenString(user.(*User))
+	actualSubClaim, err := parseJWT("Bearer: "+validToken, "sub")
+	if actualSubClaim != name {
+		t.Errorf("actualSubClaim: %v, expectedSubClaim: %v", actualSubClaim, name)
+	}
+
 	expiredToken := "Bearer: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MDEyNTg2NjgsIm5iZiI6MTUwMDcxODY2OCwic3ViIjoibmljayJ9.lNSCEYI9Ij7XLlOB4yOq8Ezd1pQeMojmuqeOa4f3LwY"
-	_, err := parseJWT(expiredToken, "sub")
+	_, err = parseJWT(expiredToken, "sub")
 	if err.Error() != "signature is invalid" {
 		t.Errorf("actualErr: %v, expected: signature is invalid", err.Error())
 	}
 
+	_, err = generateTokenString(user.(*User))
+	if err != nil {
+		t.Errorf("err: %v", err)
+	}
 }
 
 func TestIDFromToken(t *testing.T) {
-
+	name := "thisWillBeTheSub"
+	user := NewUser(name)
+	validToken, _ := generateTokenString(user.(*User))
+	actualSub, _ := IDFromToken("Bearer: " + validToken)
+	if actualSub != name {
+		t.Errorf("actualSub: %v, expectedSub: %v", actualSub, name)
+	}
 }
