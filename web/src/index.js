e48409bda52942a5f7e380be5bb0f4a45389758f
Luke Vear <vearluke@googlemail.com>
2018-10-30 18:56:08 +0000

    dog tutorial

--- web/src/index.js
+++ web/src/index.js
@@ -1,15 +1,34 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+// import registerServiceWorker from "./registerServiceWorker";
+
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 import { Provider } from 'react-redux'
-import App from './components/App'
-import reducer from './reducers'
 
-const store = createStore(reducer)
+import { reducer } from './redux'
+import { watcherSaga } from './sagas'
 
-render(
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware()
+
+// dev tools middleware
+// const reduxDevTools =
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+// create a redux store with our reducer above and middleware
+let store = createStore(reducer, applyMiddleware(sagaMiddleware))
+
+// run the saga
+sagaMiddleware.run(watcherSaga)
+
+ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
 )
+
+// registerServiceWorker();
